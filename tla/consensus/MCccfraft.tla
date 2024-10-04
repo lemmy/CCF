lemmy/CCF

commit d392be68ecd29ae358715656db20755cc61930ee

TermCount

tla/consensus/MCccfraft.tla
@@ -143,6 +143,6 @@ DebugNotTooManySigsInv ==
 
 MCRefinementToAbsProp == MappingToAbs(StartTerm..StartTerm + TermCount)!AbsSpec
 
-ABSExtend(i) == MappingToAbs(StartTerm..MaxTermLimit+1)!ExtendAxiom(i)
-ABSCopyMaxAndExtend(i) == MappingToAbs(StartTerm..MaxTermLimit+1)!CopyMaxAndExtendAxiom(i)
+ABSExtend(i) == MappingToAbs(StartTerm..StartTerm + TermCount)!ExtendAxiom(i)
+ABSCopyMaxAndExtend(i) == MappingToAbs(StartTerm..StartTerm + TermCount)!CopyMaxAndExtendAxiom(i)
 ===================================

commit 516ac822a7971736df5aabe12e79ab8eacd74643

Fix logical merge error introduced with https://github.com/microsoft/CCF/pull/6509/commits/d392be68ecd29ae358715656db20755cc61930ee
Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

tla/consensus/MCccfraft.tla
@@ -173,8 +173,8 @@ PostConditions ==
 ----
 \* Refinement
 
-MCRefinementToAbsProp == MappingToAbs(StartTerm..StartTerm + TermCount)!AbsSpec
+MCRefinementToAbsProp == MappingToAbs(StartTerm..StartTerm+1 + TermCount)!AbsSpec
 
-ABSExtend(i) == MappingToAbs(StartTerm..StartTerm + TermCount)!ExtendAxiom(i)
-ABSCopyMaxAndExtend(i) == MappingToAbs(StartTerm..StartTerm + TermCount)!CopyMaxAndExtendAxiom(i)
+ABSExtend(i) == MappingToAbs(StartTerm..StartTerm+1 + TermCount)!ExtendAxiom(i)
+ABSCopyMaxAndExtend(i) == MappingToAbs(StartTerm..StartTerm+1 + TermCount)!CopyMaxAndExtendAxiom(i)
 ===================================